1dfb0af8845026c5998cfce799dbff3e1c0f86cd
nisha <nishantsingh187@gmail.com>
2022-12-08 23:29:37 +0530

    Employee Wage And Saving Total Wage By Company

--- src/BridgeLabz/EmpWage.java
+++ src/BridgeLabz/EmpWage.java
@@ -2,6 +2,11 @@ package BridgeLabz;
 
 public class EmpWage {
     public static void main(String[] args) {
-        System.out.println("Enter a Employee Wage Computation Program");
+        EmpWageUC9 TATA = new EmpWageUC9("TATA", 10, 20, 100);
+        EmpWageUC9 Reliance = new EmpWageUC9("Reliance", 20, 22, 90);
+        TATA.calculateWage();
+        System.out.println(TATA);
+        Reliance.calculateWage();
+        System.out.println(Reliance);
     }
 }
